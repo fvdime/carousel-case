6faf7e86f31529bd4daeea5ad06ccdf9d65d02b5
fvdime <73942727+fvdime@users.noreply.github.com>
2025-03-21 22:42:45 +0300

    revise

--- script.js
+++ script.js
@@ -49,7 +49,7 @@
   const buildCSS = () => {
     const css = `
       body {
-        font-family: 'Quicksand', sans-serif;
+        font-family: Quicksand-Bold, sans-serif;
         margin: 20px;
         background: #fff;
       }
@@ -319,7 +319,7 @@
       const isFavorite = favorites.includes(product.id);
       const productCard = `
         <div class="product-card" data-id="${product.id}" data-url="${product.url}">
-          <span class="heart-icon ${isFavorite ? "favorite" : ""}">
+          <span class="heart-icon ${isFavorite && "favorite"}">
             <svg
               aria-hidden="true"
               fill="currentColor"
@@ -336,11 +336,11 @@
             <h3><b>${product.brand} -</b> ${product.name}</h3>
             ${Math.round(((product.original_price - product.price) / product.original_price) * 100) > 0 ? `<div class="price-container">
               <div>
-                ${product.original_price ? `<span class="original-price">${product.original_price} TL</span>` : ""}
-                ${product.original_price ? `<span class="discount">${Math.round(((product.original_price - product.price) / product.original_price) * 100)}% <svg aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+                ${product.original_price && `<span class="original-price">${product.original_price} TL</span>`}
+                ${product.original_price && `<span class="discount">${Math.round(((product.original_price - product.price) / product.original_price) * 100)}% <svg aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
                     <path fill="currentColor" d="m18.774 8.245-.892-.893a1.5 1.5 0 0 1-.437-1.052V5.036a2.484 2.484 0 0 0-2.48-2.48H13.7a1.5 1.5 0 0 1-1.052-.438l-.893-.892a2.484 2.484 0 0 0-3.51 0l-.893.892a1.5 1.5 0 0 1-1.052.437H5.036a2.484 2.484 0 0 0-2.48 2.481V6.3a1.5 1.5 0 0 1-.438 1.052l-.892.893a2.484 2.484 0 0 0 0 3.51l.892.893a1.5 1.5 0 0 1 .437 1.052v1.264a2.484 2.484 0 0 0 2.481 2.481H6.3a1.5 1.5 0 0 1 1.052.437l.893.892a2.484 2.484 0 0 0 3.51 0l.893-.892a1.5 1.5 0 0 1 1.052-.437h1.264a2.484 2.484 0 0 0 2.481-2.48V13.7a1.5 1.5 0 0 1 .437-1.052l.892-.893a2.484 2.484 0 0 0 0-3.51Z"></path>
                     <path fill="#fff" d="M8 13a1 1 0 0 1-.707-.293l-2-2a1 1 0 1 1 1.414-1.414l1.42 1.42 5.318-3.545a1 1 0 0 1 1.11 1.664l-6 4A1 1 0 0 1 8 13Z"></path>
-                </svg></span>` : ""}
+                </svg></span>`}
               </div>
               <span class="price">${product.price} TL</span>
             </div>` : `<span class="price">${product.price} TL</span>`}
